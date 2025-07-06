e3085855666e8a2d9f1f7104feb0ad1380395c5d
Thijs De Vries <thijs.s.devries@gmail.com>
2025-07-06 17:56:14 +0200

    Serviceworker 2

--- serviceworker.js
+++ serviceworker.js
@@ -3,9 +3,8 @@ self.addEventListener('install', event => {
     caches.open('my-cache').then(cache => {
       return cache.addAll([
         '/',
-        '/index.html',
-        '/style.css',
-        '/script.js'
+        'index.html',
+        'assets/logo64.png',
       ]);
     })
   );
